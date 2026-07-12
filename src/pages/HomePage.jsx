c20d95934c41d913a24c5b1a84c093a4f6c147e7
mustafaexce-creator <mustafa.exce@gmail.com>
2026-07-12 05:58:28 +0300

    perf: code-split below-the-fold homepage components to further reduce initial JS load

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
+import { lazy, Suspense } from 'react'
 import Hero from '../components/home/Hero'
 import StatsBar from '../components/home/StatsBar'
 import Services from '../components/home/Services'
-import WhyUs from '../components/home/WhyUs'
-import PortfolioPreview from '../components/home/PortfolioPreview'
-import Testimonials from '../components/home/Testimonials'
-import CTABanner from '../components/home/CTABanner'
+
+const WhyUs = import.meta.env.SSR ? (await import('../components/home/WhyUs')).default : lazy(() => import('../components/home/WhyUs'))
+const PortfolioPreview = import.meta.env.SSR ? (await import('../components/home/PortfolioPreview')).default : lazy(() => import('../components/home/PortfolioPreview'))
+const Testimonials = import.meta.env.SSR ? (await import('../components/home/Testimonials')).default : lazy(() => import('../components/home/Testimonials'))
+const CTABanner = import.meta.env.SSR ? (await import('../components/home/CTABanner')).default : lazy(() => import('../components/home/CTABanner'))
 
 export default function HomePage() {
   return (
@@ -12,10 +14,13 @@ export default function HomePage() {
       <Hero />
       <StatsBar />
       <Services />
-      <WhyUs />
-      <PortfolioPreview />
-      <Testimonials />
-      <CTABanner />
+      <Suspense fallback={null}>
+        <WhyUs />
+        <PortfolioPreview />
+        <Testimonials />
+        <CTABanner />
+      </Suspense>
     </>
   )
 }
+
